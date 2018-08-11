2040d3b3b1e081cf308c11245ceb48857f977732
Eric Martindale <eric@ericmartindale.com>
2018-08-10 23:20:06 -0700

    Re-format app.js, use strict mode, export app

--- app.js
+++ app.js
@@ -1,57 +1,30 @@
-    var Maki = require('maki');
-    var app = new Maki({
+'use strict';
+
+var Maki = require('maki');
+var app = new Maki({
   service: {
     name: 'teabot'
   }
 });
 
- app.define('Tea', {
+app.define('Tea', {
   attributes: {
-    tea: {
-      type: String,
-      max: 70
-},
-    country: {
-      type: String,
-      max: 70
-    },
-    province: {
-      type: String,
-      max: 70
-},
-    city: {
-      type: String,
-      max: 70
-    },
-    elevation: {
-      type: String,
-      max: 70
-},
-    date_of_harvest: {
-      type: String,
-      max: 70
-    },
-    company: {
-      type: String,
-      max: 70
-},
-    tea_master: {
-      type: String,
-      max: 70
-    }
+    tea:             { type: String, max: 70 },
+    country:         { type: String, max: 70 },
+    province:        { type: String, max: 70 },
+    city:            { type: String, max: 70 },
+    elevation:       { type: String, max: 70 },
+    date_of_harvest: { type: String, max: 70 },
+    company:         { type: String, max: 70 },
+    tea_master:      { type: String, max: 70 }
   }
 });
 
 app.define('Person', {
   attributes: {
-    name: {
-      type: String,
-      max: 70
-     },
-  description: {
-      type: String,
-      max: 70
-     }
-   }
+    name:        { type: String, max: 70 },
+    description: { type: String, max: 70 }
+  }
 });
-    app.start();
+
+module.exports = app.start();
