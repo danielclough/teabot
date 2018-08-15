d0f25042d30bcaa6787b8aa1fba976458c098d51
Eric Martindale <eric@ericmartindale.com>
2018-08-15 14:08:10 -0700

    Add Resource.create() to import tool

--- tools/import-csv.js
+++ tools/import-csv.js
@@ -8,11 +8,23 @@ const TeaBot = require('../lib/teabot');
 async function doImport (file) {
   if (!fs.existsSync(file)) return console.error(`File does not exist: ${file}`);
 
+  let bot = new TeaBot();
   let parser = csv.parse({
     columns: true
-  }, function (err, data) {
+  }, function (err, teas) {
     if (err) console.error(err);
-    console.log(data);
+
+    // TODO: use Fabric's logs rather than console
+    console.log(`CSV parsed, has ${teas.length} entries.  Beginning import...`);
+
+    for (let i = 0; i < teas.length; i++) {
+      let result = bot.app.resources.Tea.create(teas[i], function (err, obj) {
+        console.log(`Callback: ${err} ${obj}`);
+      });
+      console.log(`Import: ${result}`, teas[i]);
+    }
+
+    console.log(`CSV import finished!`);
   });
 
   fs.createReadStream(file).pipe(parser);
