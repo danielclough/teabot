6323167df07659f1e90c483933de81655214fa97
danielclough <danielclough@gmail.com>
2018-08-14 22:48:38 -0700

    thanks dant!

--- tools/import-csv.js
+++ tools/import-csv.js
@@ -1,7 +1,8 @@
 var csv = require('csv');
 var fs = require('fs');
 
-var data = fs.readFileSync('tea.csv');
-csv.parse(data, { columns: true }, function (err, content) {
-  console.log('Row:', content);
+var parser =csv.parse({columns: true}, function(err, data){
+  console.log(data);
 });
+
+fs.createReadStream(__dirname+'/tea.csv').pipe(parser);
